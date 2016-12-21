f5514734a03d36ce38ce3f0b7d23a714c38f2ad9
Heather Sanders <hlsander@lou.sanpegg.local>
2016-12-21 11:30:58 -0500

    deleted:    change_password.cgi 	deleted:    chkold.php 	deleted:    cities.sql 	deleted:    config.php 	deleted:    dedup.cgi 	deleted:    display.cgi 	deleted:    edit.cgi 	deleted:    edit.php 	deleted:    elf-orig.php 	deleted:    elf.php 	deleted:    error_log 	deleted:    form_agency.cgi 	deleted:    form_client.cgi 	deleted:    formvars.php 	deleted:    getagency.php 	deleted:    getaginfo.php 	deleted:    header.php 	deleted:    index.php 	deleted:    login.cgi 	deleted:    main.cgi 	deleted:    main.css 	deleted:    passwdscreen.cgi 	deleted:    query.txt 	deleted:    query2.txt 	deleted:    query3.txt 	deleted:    sharing-tree-logo.jpg 	deleted:    sharingt_sharingtreeDB.sql 	deleted:    stdbstructures.xlsx 	deleted:    submit_change.cgi 	deleted:    submit_form_agency.cgi 	deleted:    submit_form_client.cgi 	deleted:    vedit.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-  //MySQL Connection Info 
-   $db_host = "localhost";
-   $db = "sharingt_sharingtreeDB";
-   $db_user = "sharingtree";
-   $db_pass = "N.y$;9qk@DHr";
-?>
--- passwdscreen.cgi
+++
@@ -1,1 +0,0 @@
-#INSERT INTO Account (userID, password,Agency_Name) Values (user128, pass111, West Midland Family Center)
